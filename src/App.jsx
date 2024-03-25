0e228b06fd994235e4c6e1df29183663ac850c0d
Gabriel Pessoni <gabriel.pessoni@gmail.com>
2024-03-25 00:35:15 -0300

    not found

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import { UserStorage } from "./Contexts/UserContext";
 import User from "./Components/User/User";
 import Photo from "./Components/Feed/Photo/Photo";
 import ProtectedRoute from "./Components/Helpers/ProtectedRoute";
+import UserProfile from "./Components/User/UserProfile";
+import NotFound from "./Components/NotFound";
 
 const App = () => {
   return (
@@ -28,6 +30,8 @@ const App = () => {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -6,6 +6,7 @@ import { Routes, Route } from "react-router-dom";
 import Feed from "../Feed/Feed";
 import UserPhotoPost from "./UserPhotoPost";
 import UserStats from "./UserStats";
+import NotFound from "../NotFound";
 
 const User = () => {
   const { data } = React.useContext(UserContext);
@@ -16,6 +17,7 @@ const User = () => {
         <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStats />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </section>
   );
